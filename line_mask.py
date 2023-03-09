d7fcfbc1a23a7df7c93ee8380e8ae420d0f1cc6c
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-09 18:34:27 +0900

    Add files via upload

--- line_mask.py
+++ line_mask.py
@@ -41,21 +41,35 @@ def draw_circle(event,x,y, flags, param):
             ymax = y + size
 
             cv2.rectangle(original_image, (xmin,ymin), (xmax, ymax), (255, 255, 255), -1)
-            cv2.rectangle(mask_image, (xmin,ymin), (xmax, ymax), (255, 255, 255), -1)
+            cv2.rectangle(mask_image, (xmin,ymin), (xmax, ymax), 255, -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         
 original_image = cv2.imread(img_path)
-mask_image = np.zeros(original_image.shape)
+mask_image = np.zeros(original_image.shape[0:2])
 
-cv2.namedWindow('image')
+width, height = original_image.shape[0:2]
+position_x = 100
+position_y = 100
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', width, height)
+cv2.moveWindow('image', position_x, position_y)
 cv2.setMouseCallback('image', draw_circle)
 
 while True:  
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('s'):
+        cv2.imwrite(f'{img_fname_no_ext}_mask.png', mask_image)
+    elif key == 27:
         break
+    elif key == ord('u'):
+        position_y -= 200
+        cv2.moveWindow('image', position_x, position_y)
+    elif key == ord('d'):
+        position_y += 200
+        cv2.moveWindow('image', position_x, position_y)
     cv2.imshow('image',original_image)
-
-cv2.imwrite(f'{img_fname_no_ext}_mask.png', mask_image)
+    
 cv2.destroyAllWindows()
