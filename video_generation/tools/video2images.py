6502b74a75588aea6febc918f54f060ac7a53df2
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-04-06 17:44:19 +0900

    Add files via upload

--- video_generation/tools/video2images.py
+++ video_generation/tools/video2images.py
@@ -1,5 +1,5 @@
 import cv2
-import os 
+import os
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -8,9 +8,22 @@ parser.add_argument(
     type=str,
     help='original video'
 )
+parser.add_argument(
+    '--rembg',
+    type=str,
+    choices=["u2net", "u2netp", "u2net_human_seg", "u2net_cloth_seg", "silueta", "isnet-general-use"],
+    help='rembg model'
+)
 opt = parser.parse_args()
 
-os.makedirs('images', exist_ok=True)
+os.makedirs('rembg_images', exist_ok=True)
+
+if opt.rembg:
+    from rembg import remove, new_session
+    save_folder = f'{opt.rembg}_images'
+else:
+    save_folder = 'images'
+os.makedirs(save_folder, exist_ok=True)
 
 cap = cv2.VideoCapture(opt.video)
 
@@ -20,8 +33,11 @@ while True:
 
     if ret == False:
         break
+    
+    if opt.rembg:
+        frame = remove(frame, session=new_session(opt.rembg))
 
-    cv2.imwrite(os.path.join('images', f'{frame_no}.png'), frame)
+    cv2.imwrite(os.path.join(save_folder, f'{frame_no}.png'), frame)
     frame_no += 1
 
 cap.release()
