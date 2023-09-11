1191e8422bbb618164924a182098b4bac6e518c9
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-09-11 22:57:56 +0900

    Add files via upload

--- video_generation/tools/gif2images.py
+++ video_generation/tools/gif2images.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import os
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -9,10 +9,15 @@ parser.add_argument(
 )
 opt = parser.parse_args()
 
-gif_images = Image.open(opt.gif)
+gif_path = opt.gif
+
+gif_images = Image.open(gif_path)
+
+save_folder = os.path.splitext(os.path.basename(gif_path))[0]
+os.makedirs(save_folder, exist_ok=True)
 
 for i in range(gif_images.n_frames):
     gif_images.seek(i)
-    gif_images.save(f"{i}.png")
+    gif_images.save(os.path.join(save_folder, f"{i}.png"))
 
 
