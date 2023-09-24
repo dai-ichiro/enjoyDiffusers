98d38020551d4c3e014a2c96d0b306af833b7e84
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-09-25 08:50:38 +0900

    Add files via upload

--- video_generation/tools/images2video.py
+++ video_generation/tools/images2video.py
@@ -14,6 +14,17 @@ parser.add_argument(
     default = 8,
     help='frame per second'
 )
+parser.add_argument(
+    '--start',
+    type=int,
+    default = 0,
+    help='number of first frame'
+)
+parser.add_argument(
+    '--length',
+    type=int,
+    help='frame count'
+)
 opt = parser.parse_args()
 
 images_list = glob.glob(f'{opt.folder}/*.png')
@@ -23,8 +34,15 @@ h, w, _ = (cv2.imread(images_list[0])).shape
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
 out = cv2.VideoWriter('output.mp4',fourcc, opt.fps, (w, h))
 
-for i in range(len(images_list)):
-    image = cv2.imread(os.path.join(opt.folder, f'{i}.png'))
+start_frame = opt.start
+if opt.length is None:
+    length = len(images_list)
+else:
+    length = opt.length
+
+for i in range(length):
+    #image = cv2.imread(os.path.join(opt.folder, f'{i}.png'))
+    image = cv2.imread(os.path.join(opt.folder, f'{i+start_frame}.png'))
     out.write(image)
 
 out.release()
