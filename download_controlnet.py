177ac553987aa0cb3885b4faee3c01ab8ebf24fd
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-01-16 08:15:28 +0900

    Add files via upload

--- download_controlnet.py
+++ download_controlnet.py
@@ -2,12 +2,13 @@ import os
 from huggingface_hub import hf_hub_download
 from huggingface_hub import snapshot_download
 
-repo_id="lllyasviel/control_v11p_sd15_openpose"
+repo_id="lllyasviel/control_v11f1p_sd15_depth"
 folder = os.path.basename(repo_id)
 
 hf_hub_download(
     repo_id=repo_id, 
     filename="diffusion_pytorch_model.safetensors",
+    #filename="diffusion_pytorch_model.bin",
     local_dir=folder,
     local_dir_use_symlinks=False	
 )
