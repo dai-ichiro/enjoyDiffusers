7443c89c2bdb9a69a686c8331cebc93afc256101
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-04-23 12:11:38 +0900

    Add files via upload

--- stack.py
+++ stack.py
@@ -1,5 +1,54 @@
 import numpy as np
 from PIL import Image
+from typer import Typer, Option
+from typing import List
+
+# アスペクト比を固定して、幅が指定した値になるようリサイズする。
+def scale_to_width(img, width):
+    height = round(img.height * width / img.width)
+    return img.resize((width, height))
+
+# アスペクト比を固定して、高さが指定した値になるようリサイズする。
+def scale_to_height(img, height):
+    width = round(img.width * height / img.height)
+    return img.resize((width, height))
+
+app = Typer()
+
+@app.command()
+def main(
+    imagepath_list: List[str],
+    jpg: bool=Option(default=False, help="png or jpg"),
+    vertical: bool=Option(default=False, help="horizontal or vertical")
+):
+ 
+    image_list =[Image.open(x).convert("RGB") for x in imagepath_list]
+
+    if vertical:
+        width = image_list[0].width
+        image_list = [scale_to_width(x, width) for x in image_list]
+        array_list = [np.array(x) for x in image_list]
+        one_array = np.vstack(tuple(array_list))
+    else:
+        height = image_list[0].height
+        image_list = [scale_to_height(x, height) for x in image_list]
+        array_list = [np.array(x) for x in image_list]
+        one_array = np.hstack(tuple(array_list))
+        
+    pil_image = Image.fromarray(one_array)
+
+    if jpg:
+        pil_image.save('stackimage.jpg')
+    else:
+        pil_image.save('stackimage.png')
+
+if __name__=="__main__":
+    app()
+
+''' 
+# old script
+import numpy as np
+from PIL import Image
 import argparse
 
 # アスペクト比を固定して、幅が指定した値になるようリサイズする。
@@ -50,3 +99,4 @@ if args.jpg:
     pil_image.save('stackimage.jpg')
 else:
     pil_image.save('stackimage.png')
+'''
